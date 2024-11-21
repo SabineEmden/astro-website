a2c0f655e1a206541086a9fc2a2539be2720675b
Sabine Emden <EmdenSabine@gmail.com>
2024-11-21 13:58:08 -0500

    Add RSS feed for articles with full content

--- src/pages/articles/rss.xml.js
+++ src/pages/articles/rss.xml.js
@@ -5,17 +5,17 @@ import MarkdownIt from "markdown-it";
 const parser = new MarkdownIt();
 
 export async function GET(context) {
-  const notes = await getCollection("notes");
+  const articles = await getCollection("articles");
   return rss({
-    title: "Sabine Emden's Notes",
-    description: "Notes published on Sabine Emden's personal website",
+    title: "Sabine Emden's Articles",
+    description: "Articles published on Sabine Emden's personal website",
     site: context.site,
-    items: notes.map((note) => ({
-      ...note.data,
+    items: articles.map((article) => ({
+      ...article.data,
       // Compute RSS link from post `slug`
-      link: `/notes/${note.slug}/`,
+      link: `/articles/${article.slug}/`,
       // Note: this will not process components or JSX expressions in MDX files.
-      content: sanitizeHtml(parser.render(note.body), {
+      content: sanitizeHtml(parser.render(article.body), {
         allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
       }),
     })),
