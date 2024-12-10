ae2ec2e92ea319c21dad029090b4d6f75f2c15a8
Sabine Emden <EmdenSabine@gmail.com>
2024-12-09 21:06:27 -0500

    Change references from `slug` to `id`

--- src/pages/articles/rss.xml.js
+++ src/pages/articles/rss.xml.js
@@ -10,7 +10,7 @@ export async function GET(context) {
     items: articles.map((article) => ({
       ...article.data,
       // Compute RSS link from post `slug`
-      link: `/articles/${article.slug}/`,
+      link: `/articles/${article.id}/`,
     })),
   });
 }
--- src/pages/notes/rss.xml.js
+++ src/pages/notes/rss.xml.js
@@ -10,7 +10,7 @@ export async function GET(context) {
     items: notes.map((note) => ({
       ...note.data,
       // Compute RSS link from post `slug`
-      link: `/notes/${note.slug}/`,
+      link: `/notes/${note.id}/`,
     })),
   });
 }
