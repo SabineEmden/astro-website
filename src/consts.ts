b88a193140428c0b7701717c1a82cfffb29d06ad
Sabine Emden <EmdenSabine@gmail.com>
2025-09-04 14:18:53 -0400

    Update site title and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE =
-  "Sabine Emden | Aspiring web developer and former chemist";
+export const SITE_TITLE = "Sabine Emden | My personal website";
 export const SITE_DESCRIPTION =
-  "What I bring from chemistry to software development is a systematic approach to problem solving and the grit to not give up easily.";
+  "This is my home on the World Wide Web, where I publish notes and articles on a variety of topics that interest me.";
